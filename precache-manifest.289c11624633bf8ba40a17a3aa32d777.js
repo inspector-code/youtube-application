86ca129f8466ac7a1b4bb0c7c876d35c278d2e55
bamboleylo <tsybulko.artem@gmail.com>
2020-08-26 13:40:07 +0300

    Updates

---
+++ precache-manifest.289c11624633bf8ba40a17a3aa32d777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb207d796d05b66f848b5a17cd1b395",
+    "url": "/youtube-application/index.html"
+  },
+  {
+    "revision": "875bf349c349fcb32e7d",
+    "url": "/youtube-application/static/css/main.61557ee8.chunk.css"
+  },
+  {
+    "revision": "c9d401b90cb5362ab5f4",
+    "url": "/youtube-application/static/js/2.4f694e07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/youtube-application/static/js/2.4f694e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875bf349c349fcb32e7d",
+    "url": "/youtube-application/static/js/main.cf9b0949.chunk.js"
+  },
+  {
+    "revision": "849abbd6f30d01ca4fdb",
+    "url": "/youtube-application/static/js/runtime-main.510e1933.js"
+  }
+]);
